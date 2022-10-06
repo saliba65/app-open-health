9fe6daf801df1257716d11f42ff51b969c27f6fe
Lucas Saliba <lucassaliba10@gmail.com>
2022-10-06 14:41:09 -0300

    🐛 Fix import App.js

--- app-open-health/src/index.js
+++ app-open-health/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./pages/Home/App";
 import { Provider } from "react-redux";
 import { store } from "./store/index.js";
 
