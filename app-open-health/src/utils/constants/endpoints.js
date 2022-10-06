0b92593e21d3f7f198eeb095d93294f5de3ddf27
Lucas Saliba <lucassaliba10@gmail.com>
2022-10-06 19:32:48 -0300

    ✅ Update constants

--- app-open-health/src/utils/constants/endpoints.js
+++ app-open-health/src/utils/constants/endpoints.js
@@ -1,4 +1,12 @@
-export const baseURL =
-  "https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=300";
+export const base = process.env.REACT_APP_API_BASE_PATH;
 
-export const GET_DATA = `${baseURL}`;
+const version = "v1";
+const userPrefix = "users";
+const booksPrefix = "books";
+
+export const baseURL = `${base}/${version}/`;
+
+export const SIGN_IN = `${userPrefix}/auth/sign_in`;
+
+export const BOOKS = name => `${booksPrefix}?name=${name}`;
+export const BOOKS_BY_ID = id => `${booksPrefix}/${id}`;
