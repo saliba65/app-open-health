db8621a6d8f2dd7a6fc8e1b91af6bd8f2307afa3
Lucas Saliba <lucassaliba10@gmail.com>
2022-10-06 19:54:56 -0300

    👷 Config API

---
+++ app-open-health/src/services/api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { baseURL } from "constants/endpoints";
+import {
+  getAccessToken,
+  isAuthenticated,
+  ACCESS_TOKEN,
+  CLIENT,
+  UID
+} from "../lib/contexts/auth";
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
+
+api.interceptors.request.use(async initialConfig => {
+  const config = initialConfig;
+
+  if (isAuthenticated()) {
+    const headers = getAccessToken();
+
+    config.headers[ACCESS_TOKEN] = headers.accessToken;
+    config.headers[CLIENT] = headers.client;
+    config.headers[UID] = headers.uid;
+  }
+
+  return config;
+});
+
+export default api;
