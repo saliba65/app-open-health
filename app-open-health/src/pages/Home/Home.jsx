996539643a1886296a43c3b69c2f2ae675ff295a
Lucas Saliba <lucassaliba10@gmail.com>
2022-10-23 20:13:28 -0300

    📌 Apply PageTitle on Home

--- app-open-health/src/pages/Home/Home.jsx
+++ app-open-health/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Portal from "../../components/templates/Portal/Portal";
+import { PageTitle } from "../../components/core/Typography/Typography";
 import * as S from "./HomeStyle";
 
 const Home = () => {
   return (
     <Portal>
+      <PageTitle title={"Mural"} />
       <S.Container>
         <h1>Open Health</h1>
       </S.Container>
