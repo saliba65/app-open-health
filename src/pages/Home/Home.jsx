82b06e8325b60f5d4a91606969244eb1c828d192
Lucas Saliba <lucassaliba10@gmail.com>
2022-11-17 10:37:40 -0300

    ⚡️ Start clean dashboard

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -19,11 +19,11 @@ import {
 } from "recharts";
 
 const Home = () => {
-  const [glicemi, setGlicemi] = useState(true);
-  const [hemoglob, setHemoglob] = useState(true);
+  const [glicemi, setGlicemi] = useState(false);
+  const [hemoglob, setHemoglob] = useState(false);
   const [tsh, setTsh] = useState(false);
   const [t4, setT4] = useState(false);
-  const [triglicerid, setTriglicerid] = useState(true);
+  const [triglicerid, setTriglicerid] = useState(false);
   const [colesterol, setColesterol] = useState(false);
 
   const boxConfigLink = (hrefUrl, hrefTarget, msgText) =>
